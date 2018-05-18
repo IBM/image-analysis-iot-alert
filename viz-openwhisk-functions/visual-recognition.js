83a211e99390fd66312ead2661170e013117c046
Mark Sturdevant <mark.sturdevant@ibm.com>
2018-05-17 22:45:02 -0700

    Write image to a temp file for vis rec
    Closes #9
    
    The current code using a buffer keeps failing with
    "No images were specified". Writing to a temp file
    should not be necessary, but at least it works.

--- viz-openwhisk-functions/visual-recognition.js
+++ viz-openwhisk-functions/visual-recognition.js
@@ -1,5 +1,9 @@
 // Image processor code that will be sent to IoT Platform
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var uuid = require('uuid');
 
 function main(params) {
     // params coming from Cloudant
@@ -17,17 +21,19 @@ function main(params) {
 
     // Visual recognition api key from config file
     var visual_recognition = new VisualRecognitionV3({
-        api_key: apiKey,
-        version_date: "2017-10-18"
+      api_key: apiKey,
+      version_date: "2018-03-19"
     });
 
     var promise = new Promise(function(resolve, reject) {
+        var temp = path.join(os.tmpdir(), uuid.v1() + '.tmp');
+        fs.writeFileSync(temp, new Buffer(data, 'base64'));
         var params = {
-          images_file: Buffer(data)
+          images_file: fs.createReadStream(temp)
         };
 
         if(classifiers) {
-            params.classifier_ids = classifiers;
+          params.classifier_ids = classifiers;
         }
 
         // Classifiers will be set to a device in the IoT Platform
@@ -39,6 +45,11 @@ function main(params) {
             console.log(JSON.stringify(res, null, 2));
             resolve({payload : res, docId : docId, typeId : typeId, deviceId : deviceId});
           }
+          fs.unlink(params.images_file.path, function(e) {
+            if (e) {
+              console.log('Error deleting temp image file %s: %s', params.images_file.path, e);
+            }
+          });
         });
       });
     return promise;
